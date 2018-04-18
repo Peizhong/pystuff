23f2c73160405b83b13ce2f62c55270ec16c0189
wangpeizhong <wangpeizhong@9NPNT2X>
2018-04-18 10:20:38 +0800

    转换特殊字符

--- sysk.py
+++ sysk.py
@@ -2,6 +2,7 @@ import platform
 import time
 import feedparser
 import os
+import sys
 import urllib.request
 
 
@@ -9,9 +10,24 @@ def fetchRss(rss):
     source = feedparser.parse(rss)
     feeds = [{'Title': e.title, 'UpdateTime': e.updated,
               'Summary': e.summary, 'Link': e.link} for e in source.entries]
-    #byd = sorted(feeds, key=lambda s: s['UpdateTime'], reverse=True)
+    # byd = sorted(feeds, key=lambda s: s['UpdateTime'], reverse=True)
     # return byd[:5]
     return feeds[:5]
+
+
+def replace_invalid_filename_char(filename, replaced_char='_'):
+    '''Replace the invalid characaters in the filename with specified characater.
+    The default replaced characater is '_'.
+    e.g. 
+    C/C++ -> C_C++
+    '''
+    valid_filename = filename
+    invalid_characaters = '\\/:*?"<>|'
+    for c in invalid_characaters:
+        # print 'c:', c
+        valid_filename = valid_filename.replace(c, replaced_char)
+
+    return valid_filename
 
 
 def whichNew(feeds, localpath):
@@ -25,7 +41,7 @@ def whichNew(feeds, localpath):
     for rs in feeds:
         ex = False
         for f in exfile:
-            if(f.find(rs['Title']) >= 0):
+            if(f.find(replace_invalid_filename_char(rs['Title'])) >= 0):
                 ex = True
                 break
         if(ex == False):
@@ -37,14 +53,16 @@ def whichNew(feeds, localpath):
 def downloadSksy(rss, localpath):
     result = False
     try:
-        filepath = '%s/%s.mp3' % (localpath, rss['Title'])
+        encode = replace_invalid_filename_char(rss['Title'])
+        filepath = '%s/%s.mp3' % (localpath, encode)
         print("start download to %s" % (filepath))
         f = urllib.request.urlopen(rss['Link'])
         data = f.read()
         with open(filepath, "wb") as code:
             code.write(data)
         result = True
-    except:
+    except Exception as e:
+        print(e)
         result = False
     finally:
         return result
@@ -56,6 +74,8 @@ def main(downloadpath, autodown=True, hour=0):
         lastcheckDate = -1
         while True:
             localtime = time.localtime(time.time())
+            print("check pocast at %02d:%02d" %
+                  (localtime.tm_hour, localtime.tm_min))
             # 每天只下载一次
             if(localtime.tm_yday == lastcheckDate):
                 print('%s job is finished' % (time.asctime(localtime)))
@@ -64,7 +84,7 @@ def main(downloadpath, autodown=True, hour=0):
             # 每天只在指定的时间下载文件，过了就不管
             if (localtime.tm_hour == hour):
                 print('time to download new pocast')
-                #feeds = fetchRss('http://www.ifanr.com/feed')
+                # feeds = fetchRss('http://www.ifanr.com/feed')
                 feeds = fetchRss(
                     'https://feeds.megaphone.fm/stuffyoushouldknow')
                 print("recive %s pocasts" % (len(feeds)))
@@ -85,7 +105,7 @@ def main(downloadpath, autodown=True, hour=0):
     else:
         print('hello, fetching new pocasts....')
         feeds = fetchRss('http://www.ifanr.com/feed')
-        #feeds = fetchRss('https://feeds.megaphone.fm/stuffyoushouldknow')
+        # feeds = fetchRss('https://feeds.megaphone.fm/stuffyoushouldknow')
         print("recive %s pocasts" % (len(feeds)))
         newfeeds = whichNew(feeds, downloadpath)
         newfeedsLen = len(newfeeds)
@@ -98,8 +118,8 @@ def main(downloadpath, autodown=True, hour=0):
             inStr = input('select the number you want to download: ')
             try:
                 indexToDownload = int(inStr)
-                if (indexToDownload < newfeedsLen):
-                    todo = newfeeds[indexToDownload]
+                if (indexToDownload > 0 and indexToDownload <= newfeedsLen):
+                    todo = newfeeds[indexToDownload-1]
                     downloadSksy(todo, downloadpath)
                     break
                 else:
@@ -113,7 +133,10 @@ print('current os is '+curOs)
 curRealse = platform.release()
 print('current release is '+curRealse)
 if curOs == "Darwin":
-    downloadpath = r'/Users/Peizhong/Downloads/'
+    downloadpath = r'/Users/Peizhong/Downloads'
+elif curOs == "Linux":
+    downloadpath = r'/home/Peizhong/Downloads'
 else:
-    downloadpath = r'E:/Downloads/'
-main(autodown=True, hour=17, downloadpath=downloadpath)
+    downloadpath = r'E:/Downloads'
+
+main(autodown=True, hour=10, downloadpath=downloadpath)
