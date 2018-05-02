be176c111a9160f8dc2547ca45d2ff177cd79b20
wangpeizhong <wangpeizhong@9NPNT2X>
2018-05-02 09:26:12 +0800

    试下换到mysql

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -82,6 +82,14 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MYDEV',
+        'USER': 'root',
+        'PASSWORD': 'mypass',
+        'HOST': '193.112.41.28',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
