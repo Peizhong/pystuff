a29277d719264d324efb30a45b08ab6b2c98d69f
Peizhong Wang <w.xy.z@live.com>
2018-09-16 17:39:37 +0800

    config

--- myutils/config.py
+++ myutils/config.py
@@ -1,21 +1,9 @@
 config = {
     'hosts':['localhost',],
-    'celery_broker': 'amqp://localhost:5672',
+    'celery_broker': 'amqp://rabbit:5672',
     'celery_backend': 'django-db',
-    'database_sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'database_mysql': {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "pystuff",
-        "USER": "root",
-        "PASSWORD": "mypass",
-        "HOST": "localhost",
-        "PORT": "3306"
-    },
     'redis_connection':{
-        'host':'localhost',
+        'host':'redis',
         'password':'password'
     }
 }
