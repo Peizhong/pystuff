98c7383d7b6ccedc7756588e1b1ff66c5cad1190
Peizhong <w.xy.z@live.com>
2019-05-15 20:33:47 +0800

    config

--- myutils/config.py
+++ myutils/config.py
@@ -1,11 +1,11 @@
 config = {
-    'hosts': ['localhost', '192.168.1.100', '192.168.1.108'],
-    'file_server': 'http://192.168.1.100/',
+    'hosts': ['localhost', '193.112.41.28'],
+    'file_server': 'http://193.112.41.28/',
     'path': {
         'log': 'log',
         'sysk': 'downloads/sysk/'
     },
-    'celery_broker': 'pyamqp://192.168.1.108//',
+    'celery_broker': 'pyamqp://193.112.41.28//',
     'celery_backend': 'django-db',
     'redis_connection': {
         'host': 'redis',
