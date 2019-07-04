15d5a981a1c6d899cf0912fd4f89c3b9df1fd66a
peizhong <w.xy.z@live.com>
2019-07-04 11:07:44 +0800

    update lib

--- playground/num.py
+++ playground/num.py
@@ -1,4 +1,13 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 a = np.array([1,2,3])
 print(a)
+
+x = np.arange(1,11) 
+y =  2  * x +  5 
+plt.title("Matplotlib demo") 
+plt.xlabel("x axis caption") 
+plt.ylabel("y axis caption") 
+plt.plot(x,y)
+plt.show()
